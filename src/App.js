4222b6733f0823a6d1bbf2426fe4e99720348766
lino tom <linotom04@gmail.com>
2024-07-22 22:08:27 +0530

    DeleteSubsribersPage Created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddSubscribersPage from './components/AddSubscribersPage';
+import DeleteSubscribersPage from './components/DeleteSubscribersPage';
 
 function App() {
   return (
     <div>
       <AddSubscribersPage/>
+      <DeleteSubscribersPage/>
     </div>
   );
 }
