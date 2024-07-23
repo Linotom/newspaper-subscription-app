2a3149a44526c144874980313fed4c13793b484f
lino tom <linotom04@gmail.com>
2024-07-23 07:35:45 +0530

    Navbar Created

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import AddSubscribersPage from './components/AddSubscribersPage';
 import DeleteSubscribersPage from './components/DeleteSubscribersPage';
 import SearchSubscribersPage from './components/SearchSubscribersPage';
 import ViewSubscribersPage from './components/ViewSubscribersPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+    
       <AddSubscribersPage/>
       <DeleteSubscribersPage/>
       <SearchSubscribersPage/>
--- src/components/ViewSubscribersPage.jsx
+++ src/components/ViewSubscribersPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const ViewSubscribersPage = () => {
   return (
     <div>
+        <Navbar/>
         <table class="table">
   <thead>
     <tr>
